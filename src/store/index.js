68cdfa1ee56d12949f3b9a7ccc80d8837cb6f6ec
Lisperguer <felipe.lisperguer.h@gmail.com>
2023-02-07 19:36:18 -0300

    Redux mini project

---
+++ src/store/index.js
@@ -0,0 +1,7 @@
+import { configureStore } from "@reduxjs/toolkit";
+import { counterSlice } from "./counter";
+import { authSlice } from "./auth";
+
+export const store = configureStore({
+  reducer: { counter: counterSlice.reducer, auth: authSlice.reducer },
+});
